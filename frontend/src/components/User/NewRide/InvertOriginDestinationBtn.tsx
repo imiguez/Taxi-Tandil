dfcf66f829915bc91604ca40d677414e71be3422
imiguez <zaseznano@gmail.com>
2024-05-15 15:06:19 -0300

    closes #6; Functionality depends on the environment and icon added to invert btn

--- frontend/src/components/User/NewRide/InvertOriginDestinationBtn.tsx
+++ frontend/src/components/User/NewRide/InvertOriginDestinationBtn.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { StyleSheet, Text } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
+import { StyleSheet, TouchableHighlight } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 import { rndLocation1, rndLocation2, screenWidth } from "constants/index";
 import { useMapDispatchActions } from "hooks/slices/useMapDispatchActions";
 
@@ -8,15 +8,15 @@ import { useMapDispatchActions } from "hooks/slices/useMapDispatchActions";
 export const InvertOriginDestinationBtn: FC = () => {
 
     const {invertLocations, setLocation} = useMapDispatchActions();
-    
+
     const autoAssignLocations = () => {
         setLocation(rndLocation1, 'origin');
         setLocation(rndLocation2, 'destination');
     }
 
     return (
-        <TouchableHighlight underlayColor="white" onPress={autoAssignLocations} style={styles.btn}>
-            <Text>INV</Text>
+        <TouchableHighlight underlayColor="white" onPress={process.env.DEVELOPMENT_ENV ? autoAssignLocations : invertLocations} style={styles.btn}>
+            <AntDesign name="retweet" size={24} color="black" style={styles.icon} />
         </TouchableHighlight>
     );
 }
@@ -29,5 +29,10 @@ const styles = StyleSheet.create({
         margin: 0,
         height: 60,
         width: (screenWidth - 20)*.08,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+    icon: {
+        transform: [{ rotate: '90deg' }],
+    }
 });
